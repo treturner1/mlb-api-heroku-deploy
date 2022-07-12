4b1f2eb8e607f98f1c73813a683e02a1e9660475
Robert Anderson <67164106+reanderson89@users.noreply.github.com>
2022-07-12 13:42:44 -0700

    Delete src directory

--- src/main/java/com/mlb/mlb_api/repositories/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mlb.mlb_api.repositories;
-
-import com.mlb.mlb_api.entities.Player;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerRepository extends CrudRepository<Player, Integer> {
-
-}
